1dedd86ec7bb7f96f8c4fa1aba3c82f5ed6ae97a
mihailgavriliuc <gavriliuc.mihail@gmail.com>
2025-07-28 19:35:40 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log(1);
+const button = document.querySelector('button');
+const output = document.querySelector('.output');
+
+button.addEventListener('click', function() {
+    output.innerHTML = 'Scumpa multumesc pentru manvare !!!';
+});
